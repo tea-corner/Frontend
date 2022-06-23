e717c17fd8d09cf680e73c9fce22d76ff81fe7a9
Bozhedom <c.cffc@mail.ru>
2022-06-24 02:54:04 +1000

    add style for Block component

--- tea_corner-habbits/src/components/BlockItem.vue
+++ tea_corner-habbits/src/components/BlockItem.vue
@@ -1,10 +1,36 @@
 <template>
   <section class="sectionBlocks">
     <div class="container selectionContainer">
-      <h4 class="blockName">Привычки</h4>
-      <div class="blockNewList">
-        <button class="btnBlockAdd">Добавить привычку</button>
-      </div>
+      <ul class="iii">
+        <li>
+          <h4 class="blockName">Привычки</h4>
+          <div class="blockNewList">
+            <button class="btnBlockAdd">Добавить привычку</button>
+          </div>
+        </li>
+
+        <li>
+          <h4 class="blockName">Привычки</h4>
+          <div class="blockNewList">
+            <button class="btnBlockAdd">Добавить привычку</button>
+          </div>
+        </li>
+
+        <li>
+          <h4 class="blockName">Привычки</h4>
+          <div class="blockNewList">
+            <button class="btnBlockAdd">Добавить привычку</button>
+          </div>
+        </li>
+
+        <li>
+          <h4 class="blockName">Привычки</h4>
+          <div class="blockNewList">
+            <button class="btnBlockAdd">Добавить привычку</button>
+          </div>
+        </li>
+      </ul>
+
     </div>
   </section>
 </template>
@@ -13,6 +39,13 @@
 </script>
 
 <style scoped>
+.iii {
+  display: flex;
+}
+.btnBlockAdd {
+  width: 100%;
+  background-color: #00f0ff;
+}
 
 .sectionBlocks {
   height: 810px;
@@ -24,6 +57,17 @@
 
 .blockName {
   margin-right: 203px;
-  margin-bottom: 20px;
+  margin-bottom: 10px;
+  margin-left: 16px;
+}
+
+.blockNewList {
+  padding-bottom: 500px;
+  margin-left: 12px;
+  background-color: #00f0ff;
+  margin-right: 24px;
+  width: 308px;
+  border: 4px solid black;
+  display: flex;
 }
 </style>
