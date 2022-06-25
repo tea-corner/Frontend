f47c1a893590aaab02521ad170ff35087a924689
@ThViviani <developsviviani@gmail.com>
2022-06-25 17:34:30 +1000

    add CreateItem button component

---
+++ tea_corner-habbits/src/components/UI/AddButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <a class="addButton">
+      <img src="svg/add-btn.svg" alt="addButton" />
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "add-button",
+
+  props: {
+    name: String,
+  },
+};
+</script>
+
+<style scoped>
+</style>
